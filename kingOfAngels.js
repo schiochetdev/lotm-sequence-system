0c36df4492c1cf9ebf9af1aac3fc17e14965fe1d
Gabriel Schiochet <schiochetgabriel007@gmail.com>
2026-03-02 10:39:08 -0300

    Create kingOfAngels.js

---
+++ kingOfAngels.js
@@ -0,0 +1,31 @@
+let sequence = 1;
+let sequenceOneC = 2;
+let hasUniqueness = true;
+let hasSephirot = true;
+if (sequence === 0 && hasSephitot) {
+  console.log("You are a Above The Sequence");
+} else if (sequence === 0) {
+  console.log("You are a True God");
+} else if (
+  sequence === 1 &&
+  (sequenceOneC > 1 || hasUniqueness) &&
+  hasSephirot
+) {
+  console.log("You are a King of King of Angels");
+} else if (sequence === 1 && (sequenceOneC > 1 || hasUniqueness)) {
+  console.log("You are a King of Angels");
+} else if (sequence <= 2) {
+  console.log("You are an Angel");
+} else if (sequence <= 4) {
+  console.log("You are a demigod");
+} else if (sequence <= 6) {
+  console.log(" You are a mid-sequence beyonder");
+} else if (sequence === 7) {
+  console.log(
+    "You are a mid-sequence beyonder(elderly you'd be a low-sequence beyonder"
+  );
+} else if (sequence <= 9) {
+  console.log("You are a low-sequence-beyonder");
+} else {
+  console.log("You are a normal human");
+}
